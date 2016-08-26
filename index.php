520ef1a9edd78ab579c6b73176f38c2625c5d277
Nicholas Theodore Summers <summer68@msu.edu>
2016-08-26 12:53:32 -0400

    Added Google Analytics

--- index.php
+++ index.php
@@ -247,23 +247,31 @@
         //This function allows for the contact form to submit and send mail without
         //reloading the page! :D
         $(document).ready(function() {
-          $("form").submit(function() {
+            $("form").submit(function() {
 
-            $.ajax({
-              type: "POST",
-              url: "php/mail.php",
-              data: $(this).serialize(),
-              success: function() {
-                  console.log("Made it here!");
-              }
-              });
-
-          });
-      });
+                $.ajax({
+                    type: "POST",
+                    url: "php/mail.php",
+                    data: $(this).serialize(),
+                    success: function() {
+                        console.log("Made it here!");
+                    }
+                });
+            });
+        });
         //This function closes the navigation for mobile users when clicking a link.
         function closeNav() {
             document.getElementById("nav-trigger").checked = false;
         }
+
+        //Sets up google analytics
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-83196357-1', 'auto');
+        ga('send', 'pageview');
     </script>
 
 <!-- End Document
